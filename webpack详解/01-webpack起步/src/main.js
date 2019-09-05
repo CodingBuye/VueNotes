2f5f78ffae57e5b94cfc410683423776d078688c
wenying <2515957145@qq.com>
2019-09-05 14:30:01 +0800

    webpack的基本使用过程

--- webpack详解/01-webpack起步/src/main.js
+++ webpack详解/01-webpack起步/src/main.js
@@ -1,1 +1,10 @@
+// commonjs
 const {add, multiply} = require('./mathUtil');
+console.log(add(1, 2));
+console.log(multiply(1, 2));
+
+// ES6
+import {name, age, height} from "./info";
+console.log(name);
+console.log(age);
+console.log(height);
